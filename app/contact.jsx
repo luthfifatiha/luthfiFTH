bcd970eeb6dbb1cec2fba36636ad68cc16e10b62
luthfifatiha <luthfifatiha04@gmail.com>
2024-07-03 17:21:32 +0700

    Feat: Menambahkan button to top

---
+++ app/contact.jsx
@@ -0,0 +1,17 @@
+const Contact = () => {
+    return (
+        <section id="contact" className="pt-36 pb-32 dark:bg-slate-800">
+            <div className="container">
+                <div className="w-full px-4">
+                    <div className="max-w-xl mx-auto text-center mb-16">
+                        <h4 className="font-semibold text-lg text-primary mb-2 lg:text-2xl">Contact</h4>
+                        <h2 className="font-bold text-wir text-3xl mb-4 sm:text-4xl lg:text-5xl dark:text-white">Hubungi Saya</h2>
+                        <p className="font-medium text-md text-sir md:text-lg">Jika ingin ada yang ingin disampaikan, silahkan mengisi forum berikut</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Hero from "./components/hero";
 import Portofolio from "./components/portofolio";
 import Clients from "./components/clients";
 import Blog from "./components/blog";
+import Contact from "./contact";
 
 
 export default function Home() {
@@ -16,6 +17,11 @@ export default function Home() {
       <Portofolio />
       <Clients />
       <Blog />
+      <Contact />
+
+      <a href="#home" className="hidden h-14 w-14 justify-center items-center bg-primary rounded-full fixed z-[9999] bottom-4 right-4 p-4 hover:animate-pulse" id="to-top">
+            <span className="block w-5 h-5 border-t-2 border-l-2 rotate-45 mt-2"></span>
+      </a>
     </div>
   );
 }
