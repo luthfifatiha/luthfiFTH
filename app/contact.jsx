0a1ae3abe5394203b8c9eec091720c2f38ace148
luthfifatiha <luthfifatiha04@gmail.com>
2024-07-04 10:17:48 +0700

    Feat: Menambahkan section contact

--- app/contact.jsx
+++ app/contact.jsx
@@ -9,6 +9,26 @@ const Contact = () => {
                         <p className="font-medium text-md text-sir md:text-lg">Jika ingin ada yang ingin disampaikan, silahkan mengisi forum berikut</p>
                     </div>
                 </div>
+
+                <form>
+                    <div className="w-full lg:w-2/3 lg:mx-auto">
+                        <div className="w-full mb-8 px-4">
+                            <label htmlFor="name" className="text-base text-primary font-bold">Nama</label>
+                            <input type="text" id="name" className="w-full bg-slate-200 text-wir p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
+                        </div>
+                        <div className="w-full mb-8 px-4">
+                            <label htmlFor="email" className="text-base text-primary font-bold">Email</label>
+                            <input type="email" id="email" className="w-full bg-slate-200 text-wir p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
+                        </div>
+                        <div className="w-full mb-8 px-4">
+                            <label htmlFor="message" className="text-base text-primary font-bold">Pesan</label>
+                            <input type="text" id="message" className="w-full bg-slate-200 text-wir p-3 rounded-md focus:outline-none focus:ring-primary focus:ring-1 focus:border-primary" />
+                        </div>
+                        <div className="w-full mb-8 px-4">
+                            <button className="text-base font-semibold text-white bg-primary py-2 px-4 rounded-md hover:opacity-80 hover:shadow-lg transition duration-300 lg:py-3 lg:px-8 lg:w-full lg:rounded-full">Submit</button>
+                        </div>
+                    </div>
+                </form>
             </div>
         </section>
     )
